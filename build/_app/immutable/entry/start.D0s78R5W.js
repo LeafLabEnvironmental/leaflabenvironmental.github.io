9bc73d0227c897aaf22361086ad8994202e102ab
Kai Gohegan <kai@gohegan.uk>
2025-07-22 10:13:35 +0100

    Trying to get it to work

--- build/_app/immutable/entry/start.D0s78R5W.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/i3YxOCZD.js";export{o as load_css,r as start};
--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,10 @@ const config = {
 	kit: {
 		adapter: adapter({
 			fallback: 'index.html'
-		})
+		}),
+		paths: {
+			base: '/leaflabenvironmental-github-pages'
+		}
 	}
 };
 
